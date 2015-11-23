74649e6369e3c00938e2d5b980740762ac660145
djbb7 <danielbruzual@gmail.com>
2015-11-23 07:04:13 +0100

    Remove web service url prefix

--- src/introsde/assignment2/ehealth/MyApplicationConfig.java
+++ src/introsde/assignment2/ehealth/MyApplicationConfig.java
@@ -4,7 +4,7 @@ import javax.ws.rs.ApplicationPath;
 
 import org.glassfish.jersey.server.ResourceConfig;
 
-@ApplicationPath("sdelab/resources")
+@ApplicationPath("")
 public class MyApplicationConfig extends ResourceConfig {
     public MyApplicationConfig () {
         packages("introsde.assignment2.ehealth"); // Jersey will load all the resources under this package
