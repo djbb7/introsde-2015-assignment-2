59d15f14a04c9a24a7a192afa7dd4240c288a97d
djbb7 <danielbruzual@gmail.com>
2015-11-23 07:44:45 +0100

    Comment code

--- src/client/eHealthClient.java
+++ src/client/eHealthClient.java
@@ -25,6 +25,7 @@ import javax.xml.xpath.XPathConstants;
 import javax.xml.xpath.XPathFactory;
 
 import org.glassfish.jersey.client.ClientConfig;
+import org.glassfish.jersey.client.ClientProperties;
 import org.json.JSONArray;
 import org.json.JSONObject;
 import org.w3c.dom.Document;
@@ -50,9 +51,15 @@ public class eHealthClient {
 								 +"=> Result: %s\n"
 								 +"=> HTTP Status: %d\n";
 
+	static String mediaTypeHeader = "\n################################################################\n"+
+									"#####\n"+
+									"###   Running tests with Accept and Content-type: %s\n"+
+									"#\n";
+			
 	static String testHeader = "\n################################################################\n"+
 							   "## TEST %s \n"+
 							   "#--------------------------------------------------------------\n";
+	
 	private static Person firstPerson, lastPerson, createdPerson;
 
 	//For passing one test's results to the next
@@ -73,16 +80,20 @@ public class eHealthClient {
 		//#######################
 		
 		ClientConfig clientConfig = new ClientConfig();
+		//Long timeout in case heroku server is sleeping
+		clientConfig.property(ClientProperties.CONNECT_TIMEOUT, 60*1000);
 		Client client = ClientBuilder.newClient(clientConfig);
 		service = client.target(args[0]);
 		
-		System.out.println(String.format("Working with server: %s", args[0]));
+		System.out.println(String.format("Working with server: %s\n", args[0]));
+		
 		
 		//#######################
 		//####    RUN TESTS
 		//#######################
 		
 		for(String mediaType : new String[]{MediaType.APPLICATION_XML, MediaType.APPLICATION_JSON}){
+			System.out.println(String.format(mediaTypeHeader, mediaType.toUpperCase()));
 			
 			//Read all people, save firstPerson and lastPerson 's ids
 			testStep__3_1(mediaType);
@@ -112,14 +123,20 @@ public class eHealthClient {
 			//Count measurements for person and measureType;
 			//Create new measurement;
 			//Check count increased
-			testStep__3_9(mediaType);		
+			testStep__3_9(mediaType);
+			
+			System.out.println("\n");
 		}
 	}
 
+	/**
+	 * GET all people, save first and last Person
+	 * @param mediaType
+	 */
 	private static void testStep__3_1(String mediaType){
 		System.out.println(String.format(testHeader, "3.1"));
 
-		Response r = testGeneric(1, "/person", "GET", mediaType, null);
+		Response r = testGeneric("/person", "GET", mediaType, null);
 
 		List<Person> people = r.readEntity(new GenericType<List<Person>>() {});
 		
@@ -135,28 +152,40 @@ public class eHealthClient {
 		}
 	}
 	
+	/**
+	 * GET first Person by id
+	 * @param mediaType
+	 */
 	private static void testStep__3_2(String mediaType){
 		System.out.println(String.format(testHeader, "3.2"));
 		
-		Response r = testGeneric(2, "/person/"+firstPerson.id, "GET", mediaType, null);
+		Response r = testGeneric("/person/"+firstPerson.id, "GET", mediaType, null);
 		
 		String status = (r.getStatus() == 200 || r.getStatus() == 202)? "OK" : "ERROR";
 		
 		printResult(2, "GET", "/person/"+firstPerson.id, mediaType, status, r);
 	}
 	
+	/**
+	 * PUT first Person, changing the firstname
+	 * @param mediaType
+	 */
 	private static void testStep__3_3(String mediaType){
 		System.out.println(String.format(testHeader, "3.3"));
 
 		String newName = new StringBuilder(firstPerson.firstname).reverse().toString();
 		firstPerson.firstname = newName;
-		Response r = testGeneric(3, "/person/"+firstPerson.id, "PUT", mediaType, firstPerson);
+		Response r = testGeneric("/person/"+firstPerson.id, "PUT", mediaType, firstPerson);
 		Person responsePerson = r.readEntity(Person.class);
 		String status = (responsePerson.firstname.equals(newName))? "OK" : "ERROR";
 		
 		printResult(3, "PUT", "/person/"+firstPerson.id, mediaType, status, r);
 	}
 	
+	/**
+	 * POST new Person "Chuck Norris"
+	 * @param mediaType
+	 */
 	private static void testStep__3_4(String mediaType){
 		System.out.println(String.format(testHeader, "3.4"));
 
@@ -172,7 +201,7 @@ public class eHealthClient {
 		chuck.healthProfile.add(new Measurement("weight", "78.9"));
 		chuck.healthProfile.add(new Measurement("height", "172"));
 		
-		Response r = testGeneric(4, "/person", "POST", mediaType, chuck);
+		Response r = testGeneric("/person", "POST", mediaType, chuck);
 		Person responsePerson = r.readEntity(Person.class);
 		String status = (responsePerson.id != 0 &&
 				(r.getStatus() == 200 || r.getStatus() == 201 || r.getStatus() == 202))? "OK" : "ERROR";
@@ -182,25 +211,33 @@ public class eHealthClient {
 		createdPerson = responsePerson;
 	}
 	
+	/**
+	 * DELETE "Chuck Norris" and verify the resource was deleted
+	 * @param mediaType
+	 */
 	private static void testStep__3_5(String mediaType){
 		System.out.println(String.format(testHeader, "3.5"));
 
-		Response r = testGeneric(5, "/person/"+createdPerson.id, "DELETE", mediaType, null);
+		Response r = testGeneric("/person/"+createdPerson.id, "DELETE", mediaType, null);
 		
 		String status = (r.getStatus() == 204)?"OK":"ERROR";
 		printResult(5, "DELETE", "/person/"+createdPerson.id, mediaType, status, r);
 		
-		r = testGeneric(2, "/person/"+createdPerson.id, "GET", mediaType, null);
+		r = testGeneric("/person/"+createdPerson.id, "GET", mediaType, null);
 		
 		status = (r.getStatus() == 404)? "OK" : "ERROR";
 		
 		printResult(2, "GET", "/person/"+createdPerson.id, mediaType, status, r);
 	}
 	
+	/**
+	 * GET valid measure types
+	 * @param mediaType
+	 */
 	private static void testStep__3_6(String mediaType){
 		System.out.println(String.format(testHeader, "3.6"));
 
-		Response r = testGeneric(9, "/measureTypes", "GET", mediaType, null);
+		Response r = testGeneric("/measureTypes", "GET", mediaType, null);
 		
 		MeasureTypeList m = r.readEntity(MeasureTypeList.class);
 		
@@ -210,6 +247,10 @@ public class eHealthClient {
 		measureTypes = m;
 	}
 	
+	/**
+	 * For first Person and second Person, get all measure types
+	 * @param mediaType
+	 */
 	private static void testStep__3_7(String mediaType){
 		System.out.println(String.format(testHeader, "3.7"));
 
@@ -218,7 +259,7 @@ public class eHealthClient {
 		String foundMeasureType = null;
 		for(Person p : new Person[]{firstPerson, lastPerson}){
 			for(String measureType : measureTypes.getMeasureTypes()){
-				Response r = testGeneric(6, "/person/"+p.id+"/"+measureType, "GET", mediaType, null);
+				Response r = testGeneric("/person/"+p.id+"/"+measureType, "GET", mediaType, null);
 				List<Measurement> measurements = null;
 				if(mediaType.equals(MediaType.APPLICATION_XML)){
 					measurements = r.readEntity(MeasureHistory.class).getMeasurements();
@@ -248,19 +289,27 @@ public class eHealthClient {
 		}
 	}
 	
+	/**
+	 * GET a specific measure
+	 * @param mediaType
+	 */
 	private static void testStep__3_8(String mediaType){
 		System.out.println(String.format(testHeader, "3.8"));
 
 		String path = "/person/"+foundPersonId+"/"
 				  		+foundMeasureType+"/"
 				  		+foundMeasureId;
-		Response r = testGeneric(7, path, "GET", mediaType, null);
+		Response r = testGeneric(path, "GET", mediaType, null);
 		
 		String status = (r.getStatus() == 200)? "OK" : "ERROR";
 		
 		printResult(7, "GET", path, mediaType, status, r);
 	}
 	
+	/**
+	 * POST measurement and verify it was created
+	 * @param mediaType
+	 */
 	private static void testStep__3_9(String mediaType){
 		System.out.println(String.format(testHeader, "3.9"));
 
@@ -268,7 +317,7 @@ public class eHealthClient {
 		//Calculate initial measurement count
 		String measureType = measureTypes.getMeasureTypes().get(0);
 		String pathInitial = "/person/"+firstPerson.id+"/"+measureType;
-		Response rInitial = testGeneric(6, pathInitial, "GET", mediaType, null);
+		Response rInitial = testGeneric(pathInitial, "GET", mediaType, null);
 		
 		List<Measurement> measurements = null;
 		if(mediaType.equals(MediaType.APPLICATION_XML)){
@@ -295,13 +344,13 @@ public class eHealthClient {
 							  "\"date\": \"2011-12-09\""+
 							"}";
 		}
-		Response rAdd = testGeneric(8, pathAdd, "POST", mediaType, newMeasure);
+		Response rAdd = testGeneric(pathAdd, "POST", mediaType, newMeasure);
 		status = (rAdd.getStatus() == 200 || rAdd.getStatus() == 201 || rAdd.getStatus() == 202)?
 				 "OK" : "ERROR";
 		printResult(8, "POST", pathAdd, mediaType, status, rAdd);
 		
 		//Check measure count increased
-		Response rFinal = testGeneric(6, pathInitial, "GET", mediaType, null);
+		Response rFinal = testGeneric(pathInitial, "GET", mediaType, null);
 		
 		if(mediaType.equals(MediaType.APPLICATION_XML)){
 			measurements = rFinal.readEntity(MeasureHistory.class).getMeasurements();
@@ -313,7 +362,15 @@ public class eHealthClient {
 		printResult(6, "GET", pathInitial, mediaType, status, rFinal);
 	}
 	
-	private static Response testGeneric(int reqNumber, String URIPath,  String method, String mediaType, Object requestBody){
+	/**
+	 * Generic function for performing a request and getting back the result
+	 * @param URIPath Path of request
+	 * @param method HTTP method ("GET", "POST", "PUT", or "DELETE")
+	 * @param mediaType "application/xml" or "application/json"
+	 * @param requestBody Request body to be sent, can be null.
+	 * @return
+	 */
+	private static Response testGeneric(String URIPath,  String method, String mediaType, Object requestBody){
 		Response response = null;
 		Invocation.Builder builder = service.path(URIPath)
 				.request()
@@ -338,13 +395,22 @@ public class eHealthClient {
 		} else if(method.equals("DELETE")){
 			response = builder.delete();
 		} else {
-			throw new RuntimeException("Unexpected header: "+method);
+			throw new RuntimeException("Unexpected HTTP method: "+method);
 		}
 		
 		response.bufferEntity();
 		return response;
 	}
 	
+	/**
+	 * Output request's result in pretty format.
+	 * @param reqNumber Number of request.
+	 * @param reqMethod HTTP method used ("GET", "POST", "PUT" or "DELETE")
+	 * @param path The requested URI
+	 * @param acceptType The value of the "Accept" header sent, i.e. "application/xml" or "application/json"
+	 * @param statusString "OK" or "ERROR"
+	 * @param r Response sent by the server
+	 */
 	private static void printResult(int reqNumber, String reqMethod, String path, String acceptType, String statusString, Response r){
 		int statusCode = r.getStatus();
 		String result = r.readEntity(String.class);
@@ -363,6 +429,12 @@ public class eHealthClient {
 
 	}
 	
+	/**
+	 * Given a JSON string, beautify it for printing.
+	 * @param jsonString
+	 * @param indent
+	 * @return
+	 */
 	private static String prettyJSON(String jsonString, int indent){
 		String res;
 		
@@ -380,6 +452,14 @@ public class eHealthClient {
 		return res;
 	}
 	
+	/**
+	 * Given an XML string, beautify it for printing.
+	 * Taken from: http://myshittycode.com/2014/02/10/java-properly-indenting-xml-string/
+	 * Credit to: http://stackoverflow.com/users/341508/limc
+	 * @param xml
+	 * @param indent
+	 * @return
+	 */
 	private static String prettyXML(String xml, int indent) {
 	    try {
 	        // Turn xml string into a document
